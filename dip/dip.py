49c44f93de1e5822ca5c31dda7be56bba61f2c3a
PierreCoursimault <pierrecoursi@gmail.com>
2022-01-05 10:22:07 +0100

    Update dip.py

--- dip/dip.py
+++ dip/dip.py
@@ -13,8 +13,68 @@ from skimage.metrics import peak_signal_noise_ratio
 from utils.denoising_utils import *
 
 
-def Main(input_leopold = False):
+# Load image
 
+def get_noisy_image(img_np, sigma):
+    """Adds Gaussian noise to an image.
+
+    Args: 
+        img_np: image, np.array with values from 0 to 1
+        sigma: std of the noise
+    """
+    img_noisy_np = np.clip(img_np + np.random.normal(scale=sigma, size=img_np.shape), 0, 1).astype(np.float32)
+    img_noisy_pil = np_to_pil(img_noisy_np)
+
+    return img_noisy_pil, img_noisy_np
+
+  
+def closure():
+  global i, out_avg, psrn_noisy_last, last_net, net_input
+
+  if reg_noise_std > 0:
+      net_input = net_input_saved + (noise.normal_() * reg_noise_std)
+
+  out = net(net_input)
+
+  # Smoothing
+  if out_avg is None:
+      out_avg = out.detach()
+  else:
+      out_avg = out_avg * exp_weight + out.detach() * (1 - exp_weight)
+
+  total_loss = mse(out, img_noisy_torch)
+  total_loss.backward()
+
+  psrn_noisy = peak_signal_noise_ratio(img_noisy_np, out.detach().cpu().numpy()[0]) 
+  psrn_gt    = peak_signal_noise_ratio(img_np, out.detach().cpu().numpy()[0]) 
+  psrn_gt_sm = peak_signal_noise_ratio(img_np, out_avg.detach().cpu().numpy()[0]) 
+
+  # Note that we do not have GT for the "snail" example
+  # So 'PSRN_gt', 'PSNR_gt_sm' make no sense
+  print ('Iteration %05d    Loss %f   PSNR_noisy: %f   PSRN_gt: %f PSNR_gt_sm: %f' % (i, total_loss.item(), psrn_noisy, psrn_gt, psrn_gt_sm), '\r', end='')
+  if  PLOT and i % show_every == 0:
+      out_np = torch_to_np(out)
+      plot_image_grid([np.clip(out_np, 0, 1), 
+                       np.clip(torch_to_np(out_avg), 0, 1)], factor=figsize, nrow=1)
+      print("noisy / output : ", psrn_noisy, "initial / output : ", psrn_gt, "initial / avg_output : " , psrn_gt_sm)
+
+  # Backtracking
+  if i % show_every:
+      if psrn_noisy - psrn_noisy_last < -5: 
+          print('Falling back to previous checkpoint.')
+
+          for new_param, net_param in zip(last_net, net.parameters()):
+              net_param.data.copy_(new_param.cuda())
+
+          return total_loss*0
+      else:
+          last_net = [x.detach().cpu() for x in net.parameters()]
+          psrn_noisy_last = psrn_noisy
+
+  i += 1
+  return total_loss
+
+def Main(input_leopold = False):
   # Import libs
   torch.backends.cudnn.enabled = True
   torch.backends.cudnn.benchmark =True
@@ -34,22 +94,6 @@ def Main(input_leopold = False):
   else:
     fname = 'data/F16_GT.png'
   #fname = '11b_194.png'
-
-  # Load image
-
-  def get_noisy_image(img_np, sigma):
-      """Adds Gaussian noise to an image.
-
-      Args: 
-          img_np: image, np.array with values from 0 to 1
-          sigma: std of the noise
-      """
-      
-      import numpy as np
-      img_noisy_np = np.clip(img_np + np.random.normal(scale=sigma, size=img_np.shape), 0, 1).astype(np.float32)
-      img_noisy_pil = np_to_pil(img_noisy_np)
-
-      return img_noisy_pil, img_noisy_np
 
   #img = Image.open('11b_194.png')
   #img_np = np.asarray(img)
@@ -177,56 +221,7 @@ def Main(input_leopold = False):
   psrn_noisy_last = 0
 
   i = 0
-  def closure():
-
-      global i, out_avg, psrn_noisy_last, last_net, net_input
-
-      if reg_noise_std > 0:
-          net_input = net_input_saved + (noise.normal_() * reg_noise_std)
-
-      out = net(net_input)
-
-      # Smoothing
-      if out_avg is None:
-          out_avg = out.detach()
-      else:
-          out_avg = out_avg * exp_weight + out.detach() * (1 - exp_weight)
-
-      total_loss = mse(out, img_noisy_torch)
-      total_loss.backward()
-
-      psrn_noisy = peak_signal_noise_ratio(img_noisy_np, out.detach().cpu().numpy()[0]) 
-      psrn_gt    = peak_signal_noise_ratio(img_np, out.detach().cpu().numpy()[0]) 
-      psrn_gt_sm = peak_signal_noise_ratio(img_np, out_avg.detach().cpu().numpy()[0]) 
-
-      # Note that we do not have GT for the "snail" example
-      # So 'PSRN_gt', 'PSNR_gt_sm' make no sense
-      print ('Iteration %05d    Loss %f   PSNR_noisy: %f   PSRN_gt: %f PSNR_gt_sm: %f' % (i, total_loss.item(), psrn_noisy, psrn_gt, psrn_gt_sm), '\r', end='')
-      if  PLOT and i % show_every == 0:
-          out_np = torch_to_np(out)
-          plot_image_grid([np.clip(out_np, 0, 1), 
-                           np.clip(torch_to_np(out_avg), 0, 1)], factor=figsize, nrow=1)
-          print("noisy / output : ", psrn_noisy, "initial / output : ", psrn_gt, "initial / avg_output : " , psrn_gt_sm)
-
-
-
-      # Backtracking
-      if i % show_every:
-          if psrn_noisy - psrn_noisy_last < -5: 
-              print('Falling back to previous checkpoint.')
-
-              for new_param, net_param in zip(last_net, net.parameters()):
-                  net_param.data.copy_(new_param.cuda())
-
-              return total_loss*0
-          else:
-              last_net = [x.detach().cpu() for x in net.parameters()]
-              psrn_noisy_last = psrn_noisy
-
-      i += 1
-
-      return total_loss
-
+  
   p = get_params(OPT_OVER, net, net_input)
   optimize(OPTIMIZER, p, closure, LR, num_iter)
 
