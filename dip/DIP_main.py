a98e2cc5bb89b16c639e9d2e52bac1d8962d8cf3
PierreCoursimault <pierrecoursi@gmail.com>
2022-01-07 11:16:05 +0100

    Update DIP_main.py

--- dip/DIP_main.py
+++ dip/DIP_main.py
@@ -21,7 +21,21 @@ torch.backends.cudnn.enabled = True
 torch.backends.cudnn.benchmark =True
 dtype = torch.cuda.FloatTensor
 
-# Redefine some DIP functions
+
+#Tool function
+
+def get_noisy_image(img_np, sigma):
+    """Adds Gaussian noise to an image.
+    
+    Args: 
+        img_np: image, np.array with values from 0 to 1
+        sigma: std of the noise
+    """
+    img_noisy_np = np.clip(img_np + np.random.normal(scale=sigma, size=img_np.shape), 0, 1).astype(np.float32)
+    img_noisy_pil = np_to_pil(img_noisy_np)
+
+    return img_noisy_pil, img_noisy_np
+  
 
 def Setup(img_pil, img_noisy_np, INPUT = 'noise', pad = 'reflection', input_depth = 1):
 
