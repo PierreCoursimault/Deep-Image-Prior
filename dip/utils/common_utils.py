a0de8e5d31a00241a4940d0e2a1f5347b8d4e8b1
PierreCoursimault <pierrecoursi@gmail.com>
2022-01-10 16:56:35 +0100

    Update common_utils.py

--- dip/utils/common_utils.py
+++ dip/utils/common_utils.py
@@ -152,6 +152,34 @@ def get_noise(input_depth, method, spatial_size, noise_type='u', var=1./10):
         
     return net_input
 
+def get_noise3D(input_depth, method, spatial_size, noise_type='u', var=1./10):
+    """Returns a pytorch.Tensor of size (1 x `input_depth` x `spatial_size[0]` x `spatial_size[1]` x `spatial_size[2]`) 
+    initialized in a specific way.
+    Args:
+        input_depth: number of channels in the tensor
+        method: `noise` for fillting tensor with noise; `meshgrid` for np.meshgrid
+        spatial_size: spatial size of the tensor to initialize
+        noise_type: 'u' for uniform; 'n' for normal
+        var: a factor, a noise will be multiplicated by. Basically it is standard deviation scaler. 
+    """
+    if isinstance(spatial_size, int):
+        spatial_size = (spatial_size, spatial_size, spatial_size)
+    if method == 'noise':
+        shape = [1, input_depth, spatial_size[0], spatial_size[1], spatial_size[2]]
+        net_input = torch.zeros(shape)
+        
+        fill_noise(net_input, noise_type)
+        net_input *= var            
+    elif method == 'meshgrid': 
+        assert input_depth == 2
+        X, Y, Z = np.meshgrid(np.arange(0, spatial_size[1])/float(spatial_size[1]-1), np.arange(0, spatial_size[0])/float(spatial_size[0]-1), np.arange(0, spatial_size[2])/float(spatial_size[2]-1))
+        meshgrid = np.concatenate([X[None,: , :], Y[None,: , :], Z[None, :, :]])
+        net_input=  np_to_torch(meshgrid)
+    else:
+        assert False
+        
+    return net_input
+
 def pil_to_np(img_PIL):
     '''Converts image in PIL format to np.array.
     
